92439de081b39de6b03e551f9e0c250f1f0f6616
Jean Guyomarc'h <jean@guyomarch.bzh>
2019-05-28 08:25:15 +0200

    experience/electron: make a LED blink for debug
    - RESET: changes state every second
    - DOING THE ACTION: HIGH
    - AFTER THE ACTION: blink fast

--- experience2019/electron/electron.ino
+++ experience2019/electron/electron.ino
@@ -5,20 +5,30 @@
  *      |1|8| Vcc
  * PIN3 |2|7|
  *      |3|6| PIN1
- *  GND |4|5|
+ *  GND |4|5| PIN0
  *      '---'
  */
 
 #define PIN_SENSOR 3    /* PIN3 */
 #define PIN_MOTOR 1     /* PIN1 */
+#define PIN_LED 0       /* PIN0 */
 
 /* Duration (in milliseconds) during which the motor must be turned on */
-#define MOTOR_ACTIVATION_TIME_MS 1000
+#define MOTOR_ACTIVATION_TIME_MS 30000 /* 30 s */
+
 
 static inline boolean
 detects_magnetic_field(void)
 {
   return digitalRead(PIN_SENSOR) == 0;
+}
+
+static inline void
+led_toggle(void)
+{
+  static boolean is_on = false;
+  digitalWrite(PIN_LED, (is_on) ? HIGH : LOW);
+  is_on = ! is_on;
 }
 
 void setup(void)
@@ -30,20 +40,43 @@ void setup(void)
    * must ensure a LOW signal */
   pinMode(PIN_MOTOR, OUTPUT);
   digitalWrite(PIN_MOTOR, LOW);
+
+  /* Configure the debug LED as being OFF at startup */
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
 }
 
 void loop(void)
 {
+  /* Before doing our job, we will blink slowly (change LED stage each second) */
+  static unsigned int led_delay_count = 0u;
+  static unsigned int led_trigger_count = 1000u;
+
   static boolean electron_is_triggered = false;
   if (detects_magnetic_field() && (! electron_is_triggered))
   {
     electron_is_triggered = true;
 
+    /* Stay HIGH until we are done */
+    digitalWrite(PIN_LED, HIGH);
+
     /* Turn on the motor for a given amount of time, then turn it off */
     digitalWrite(PIN_MOTOR, HIGH);
     delay(MOTOR_ACTIVATION_TIME_MS);
     digitalWrite(PIN_MOTOR, LOW);
+
+    /* We have done our job: blink much faster */
+    led_trigger_count = 250u;
   }
 
-  delay(10); /* IDLE a bit */
+  /* Control how the LED is going to be toggled */
+  if (led_delay_count >= led_trigger_count)
+  {
+    led_toggle();
+    led_delay_count = 0u;
+  }
+
+  /* IDLE - sleep for 10ms */
+  led_delay_count += 10u;
+  delay(10);
 }
